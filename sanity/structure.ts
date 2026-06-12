edf16aae5f0147bd6442fd2d706b6ba33c917960
ksegerink-creator <k.segerink@gmail.com>
2026-06-12 14:00:12 +0200

    Add Sanity Studio structure

---
+++ sanity/structure.ts
@@ -0,0 +1,25 @@
+import type {StructureResolver} from 'sanity/structure'
+
+const singleton = (S: any, type: string, title: string) =>
+  S.listItem()
+    .title(title)
+    .schemaType(type)
+    .child(S.document().schemaType(type).documentId(type).title(title))
+
+export const structure: StructureResolver = (S) =>
+  S.list()
+    .title('Audacious CMS')
+    .items([
+      singleton(S, 'siteSettings', 'Site instellingen'),
+      singleton(S, 'navigation', 'Navigatie'),
+      singleton(S, 'homePage', 'Homepage'),
+      S.divider(),
+      S.documentTypeListItem('page').title('Pagina’s'),
+      S.documentTypeListItem('service').title('Werkzaamheden'),
+      S.documentTypeListItem('market').title('Markten'),
+      S.documentTypeListItem('productGroup').title('Productgroepen'),
+      S.divider(),
+      S.documentTypeListItem('blogPost').title('Blogartikelen'),
+      S.documentTypeListItem('blogCategory').title('Blogcategorieën'),
+      S.documentTypeListItem('author').title('Auteurs')
+    ])
